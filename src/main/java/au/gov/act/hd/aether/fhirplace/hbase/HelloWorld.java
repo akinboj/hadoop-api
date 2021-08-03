31ab6412f6a25e908f99cd314194a9d9be23151e
kelskye <kelskye@gmail.com>
2021-08-03 16:35:24 +1000

    Harness for hbase to run from the command line

---
+++ src/main/java/au/gov/act/hd/aether/fhirplace/hbase/HelloWorld.java
@@ -0,0 +1,33 @@
+package au.gov.act.hd.aether.fhirplace.hbase;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.client.HBaseAdmin;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        HelloWorld hw = new HelloWorld();
+        
+        Configuration config = hw.getConfiguration();
+        try {
+            HBaseAdmin.checkHBaseAvailable(config);
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+    }
+    
+    private Configuration getConfiguration() {
+        Configuration config = HBaseConfiguration.create();
+
+        String path = this.getClass()
+          .getClassLoader()
+          .getResource("hbase-site.xml")
+          .getPath();
+        config.addResource(new Path(path));
+        return config;
+    }
+}
